690356b510a9465bce53d32db4a578ae8479d620
Pro7777tim <165313057+Pro7777tim@users.noreply.github.com>
2025-09-29 13:21:28 +0300

    Adding a recompilation

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 if [ -e "main" ]; then
-    echo "Launch begins..."
-    ./main
+    if cmp -s main.cpp _main.cpp; then
+        echo "Launch begins..."
+        ./main
+    else
+        echo "Starting recompilation..."
+        rm -f main
+        rm -f _main.cpp
+        cp main.cpp _main.cpp
+        g++ main.cpp -o main
+        echo "Launch begins..."
+        ./main
+    fi
 else
     echo "Starting compilation..."
     g++ main.cpp -o main
