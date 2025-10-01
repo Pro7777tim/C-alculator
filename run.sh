ef121fa4df0c6200b34f0041521f53c166532c5f
Pro7777tim <165313057+Pro7777tim@users.noreply.github.com>
2025-10-01 13:38:53 +0300

    Developer mode added

--- run.sh
+++ run.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+#To rebuild, change to true
+devmode=false
+#--------------------------
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -L "$SOURCE" ]; do
+    DIR="$(cd -P "$( dirname "$SOURCE" )" && pwd)"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+SCRIPT_DIR="$(cd -P "$( dirname "$SOURCE" )" && pwd)"
+cd "$SCRIPT_DIR" || exit 1
+
 if [ -e "main" ]; then
-    if cmp -s main.cpp _main.cpp; then
+    if ! $devmode; then
         echo "Launch begins..."
         ./main
     else
